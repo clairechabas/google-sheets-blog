f57077bbaddc4bea977211152d9c974c3136a789
Claire Chabas <claire.chabas@gmail.com>
2023-04-02 23:30:54 +0200

    feat: change to emoji favicon and remove useless icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,10 @@ export default function Home() {
           content="Fully functional blog using Google Sheet as database"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📑</text></svg>"
+        />
       </Head>
       <main>Coucou</main>
     </>
