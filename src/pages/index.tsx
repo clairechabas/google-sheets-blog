5965f908eee08fa00c2a53686e6138a70a4cad42
Claire Chabas <claire.chabas@gmail.com>
2023-04-02 23:27:55 +0200

    refactor: remove unused code and useless comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,4 @@
 import Head from 'next/head'
-// import Image from 'next/image'
-// import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-/**
- * TO DO LATER
- * warn  - Your project has `@next/font` installed as a dependency,
- * please use the built-in `next/font` instead. The `@next/font`
- * package will be removed in Next.js 14. You can migrate by running
- * `npx @next/codemod@latest built-in-next-font .`.
- * Read more: https://nextjs.org/docs/messages/built-in-next-font
- */
-// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -25,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>Coucou</main>
+      <main>Coucou</main>
     </>
   )
 }
